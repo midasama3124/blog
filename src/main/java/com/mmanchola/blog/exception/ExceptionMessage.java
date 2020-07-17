c683e55c3329b77e940965f80bc0dde8fe8913c4
midasama3124 <midasama3124@gmail.com>
2020-07-16 20:24:03 -0500

    Tag utilities in admin page working.

--- src/main/java/com/mmanchola/blog/exception/ExceptionMessage.java
+++ src/main/java/com/mmanchola/blog/exception/ExceptionMessage.java
@@ -3,21 +3,22 @@ package com.mmanchola.blog.exception;
 import org.springframework.util.StringUtils;
 
 public enum ExceptionMessage {
-  INVALID(" is invalid"),
-  MISSING(" is missing"),
-  UNAVAILABLE(" has already been taken"),
-  MISSING_INVALID(" is missing or invalid"),
-  MISSING_UNAVAILABLE(" is missing or has been already taken"),
-  INVALID_UNAVAILABLE(" is invalid or has already been taken"),
-  ALL(" is missing, invalid or unavailable"),
-  NOT_FOUND(" was not found");
+    INVALID("es inválido (valor mal escrito)"),
+    MISSING("hace falta (valor obligatorio)"),
+    UNAVAILABLE("ya se encuentra registrado"),
+    MISSING_INVALID("es inválido o inexistente"),
+    MISSING_UNAVAILABLE("está faltando o ya está ocupado"),
+    INVALID_UNAVAILABLE("es inválido o no está disponible"),
+    ALL("está faltando, es inválido o no está disponible"),
+    NOT_FOUND("no se encuentra registrado");
 
-  private String message;
-  ExceptionMessage(String message) {
-    this.message = message;
-  }
+    private String message;
 
-  public String getMsg(String tableField) {
-    return StringUtils.capitalize(tableField) + this.message;
-  }
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMsg(String tableField) {
+        return StringUtils.capitalize(tableField) + " " + this.message;
+    }
 }
